cb84aa4b9599f71705c0062c76e504c9a4a382ce
zeng <zeng15374396353@163.com>
2021-07-19 10:53:48 +0800

    second commit

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -9,5 +9,6 @@ package com.atguigu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello fit2!");
     }
 }
